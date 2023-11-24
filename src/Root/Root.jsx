cefbaae18c725b074330f23a05996ccfbdd96d08
Md. Rafiul Islam <rislamshaon95@gmail.com>
2023-11-24 13:09:21 +0600

    an error is fixed

---
+++ src/Root/Root.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../Components/Navbar/Navbar";
+import Footer from "../Components/Footer/Footer";
+
+
+const Root = () => {
+    return (
+        <div className="container mx-auto">
+            <Navbar></Navbar>
+            <div className="min-h-[900px]">
+            <Outlet></Outlet>
+            </div>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Root;
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,9 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
+import Root from './../Root/Root';
+import Home from "../Components/Home/Home";
 
 const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <Root></Root>,
+      children: [
+        {
+            path: "/",
+            element: <Home></Home>
+        }
+      ]
     },
   ]);
 
