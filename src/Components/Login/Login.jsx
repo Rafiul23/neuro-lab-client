23a4ef2130ed8d32bf1f90c60735216e50431943
Md. Rafiul Islam <rislamshaon95@gmail.com>
2023-11-26 01:46:44 +0600

    login page is modified

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -6,11 +6,12 @@ const Login = () => {
         <div className="hero min-h-screen bg-base-200">
             <div className="flex flex-col py-20 lg:flex-row-reverse">
                 <div className="text-center lg:w-1/2">
-                    <h1 className="text-5xl my-4 text-[#1fc281] font-bold">Login now!</h1>
+                    
                    <img src={loginImg} alt="" />
                 </div>
                 <div className="card w-1/2 max-w-md mx-auto shadow-2xl bg-base-100">
                     <form className="card-body">
+                    <h1 className="text-5xl text-center my-4 text-[#1fc281] font-bold">Login now!</h1>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
