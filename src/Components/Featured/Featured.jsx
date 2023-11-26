0570dc2157804a7f975d6d995d5c44994a8267d8
Md. Rafiul Islam <rislamshaon95@gmail.com>
2023-11-26 19:11:04 +0600

    featured tests are brought from database

--- src/Components/Featured/Featured.jsx
+++ src/Components/Featured/Featured.jsx
@@ -1,16 +1,21 @@
 import { useEffect, useState } from 'react';
 import SectionTitle from '../SectionTitle/SectionTitle';
 import TestCard from '../TestCard/TestCard';
+import useAxiosPublic from '../../Hooks/useAxiosPublic';
 
 const Featured = () => {
 
     const [featured, setFeatured] = useState([]);
+    const axiosPublic = useAxiosPublic();
 
-    useEffect(() => {
-        fetch('featured.json')
-            .then(res => res.json())
-            .then(data => setFeatured(data))
-    }, [])
+    // useEffect(() => {
+    //     fetch('featured.json')
+    //         .then(res => res.json())
+    //         .then(data => setFeatured(data))
+    // }, [])
+
+    axiosPublic.get('/featured')
+    .then(data => setFeatured(data.data))
 
     return (
         <div className='pt-10'>
